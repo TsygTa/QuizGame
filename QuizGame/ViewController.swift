ec167c6cb2a8649e6808bf5696c6a5bef1ba4cc7
TsygTa <670724@gmail.com>
2019-09-25 01:28:26 +0300

    init

---
+++ QuizGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizGame
+//
+//  Created by Tatiana Tsygankova on 25/09/2019.
+//  Copyright © 2019 Tatiana Tsygankova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
